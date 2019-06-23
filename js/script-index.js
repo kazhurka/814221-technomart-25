e445ffb2ac2df128678cfbcdd8d307289647f70d
kazhurka <42044750+kazhurka@users.noreply.github.com>
2019-06-23 14:32:18 +0300

    Доработка проекта

---
+++ js/script-index.js
@@ -0,0 +1,56 @@
+var mapLink = document.querySelector(".map-index");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".close-button");
+var indexLink = document.querySelector(".main-contacts-link");
+var indexPopup = document.querySelector(".modal-write-us");
+var indexClose = indexPopup.querySelector(".close-button");
+var form = indexPopup.querySelector(".write-us");
+var nameInput = indexPopup.querySelector(".user-name");
+var email = indexPopup.querySelector("#user-e-mail");
+var letter = indexPopup.querySelector("textarea");
+
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+
+indexLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  indexPopup.classList.add("modal-show");
+  nameInput.focus();
+});
+
+indexClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  indexPopup.classList.remove("modal-show");
+  indexPopup.classList.remove("modal-error");
+
+});
+
+window.addEventListener("keydown", function (evt) {
+
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
+    }
+    if (indexPopup.classList.contains("modal-show")) {
+      indexPopup.classList.remove("modal-show");
+    }
+  }
+});
+form.addEventListener("submit", function (evt) {
+  if (!nameInput.value || !email.value || !letter.value) {
+    evt.preventDefault();
+    indexPopup.classList.add("modal-error");
+  }
+});
+
+
