9c23417801f8e656ce73e718ec6f17189c5871f2
kazhurka <42044750+kazhurka@users.noreply.github.com>
2019-06-24 13:04:13 +0300

    Доработки

--- js/script.js
+++ js/script.js
@@ -10,6 +10,9 @@ var email = indexPopup.querySelector("#user-e-mail");
 var letter = indexPopup.querySelector("textarea");
 
 
+
+
+
 mapLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   mapPopup.classList.add("modal-show");
@@ -54,3 +57,27 @@ form.addEventListener("submit", function (evt) {
 });
 
 
+
+var innerLinks = document.querySelectorAll(".item-button-buy");
+var innerPopup = document.querySelector(".modal-cart");
+var innerClose = innerPopup.querySelector(".close-button");
+
+innerClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  innerPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (innerPopup.classList.contains("modal-show")) {
+      innerPopup.classList.remove("modal-show");
+    }
+  }
+});
+for (var i = 0; i < innerLinks.length; i++) {
+  innerLinks[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    innerPopup.classList.add("modal-show");
+
+  });
+}
